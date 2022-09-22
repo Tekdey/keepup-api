b001dc923c733504ca8aabb9f17f8f4f2a954ca0
Tekdey <devw.nbardi@gmail.com>
2022-09-22 11:06:33 +0200

    org: db actions in classes

---
+++ src/config/MongooseConfig.js
@@ -0,0 +1,21 @@
+class Mongoose {
+  constructor() {
+    this.mongoose = require("mongoose");
+    this.db = this.mongoose.connection;
+  }
+
+  connect() {
+    //Set up default mongoose connection
+    this.mongoose.connect(process.env.MONGO_URI, { useNewUrlParser: true });
+
+    //Bind connection to error event (to get notification of connection errors)
+    this.db.on(
+      "error",
+      console.error.bind(console, "MongoDB connection error:")
+    );
+
+    return this.db;
+  }
+}
+
+module.exports = Mongoose;
--- src/index.js
+++ src/index.js
@@ -2,6 +2,11 @@ const express = require("express");
 const app = express();
 const router = require("./router");
 
+// db connection
+const MongooseConfig = require("./config/MongooseConfig");
+const mongoose = new MongooseConfig();
+mongoose.connect();
+
 app.use(express.urlencoded({ extended: true }));
 app.use(express.json());
 
