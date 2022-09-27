3b41d1592328ae01fe44c4658f5d99ee1f0b0ed4
jmussardev <jmussard.dev@gmail.com>
2022-09-27 10:45:38 +0200

    + user endpoint ok

--- src/router/get.js
+++ src/router/get.js
@@ -2,5 +2,6 @@ const router = require("express").Router();
 const { user } = require("../controller");
 
 router.get("/", user.register);
+router.get("/user/:id/", user.getUser);
 
 module.exports = router;
