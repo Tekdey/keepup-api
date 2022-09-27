eb83d777c3eafa8247f8d92a2c98dc89b0497864
Tekdey <devw.nbardi@gmail.com>
2022-09-27 13:04:25 +0200

    add: create event validation middleware with joi

--- src/helper/validation/schema.js
+++ src/helper/validation/schema.js
@@ -28,5 +28,27 @@ module.exports = {
         .min(8)
         .max(14);
     },
+    event() {
+      return Joi.object({
+        name: Joi.string().required(),
+        sport: Joi.string().required(),
+        level: Joi.string(),
+        gender: Joi.string(),
+        handicap: Joi.boolean(),
+        max: Joi.number(),
+        date: Joi.date().required(),
+        // messages: Joi.array(),
+        admin: Joi.string().required(),
+        // participant: Joi.string(),
+        country: Joi.string(),
+        city: Joi.string().required(),
+        zipcode: Joi.boolean().required(),
+        longitude: Joi.string(),
+        latitude: Joi.string(),
+      })
+        .required()
+        .min(6)
+        .max(15);
+    },
   },
 };
