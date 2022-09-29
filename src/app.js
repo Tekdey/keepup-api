f67a3f1bad6cf90e2d5f39c7209bcf92b816c1de
Tekdey <devw.nbardi@gmail.com>
2022-09-29 13:27:28 +0200

    add: cors options

--- src/app.js
+++ src/app.js
@@ -1,14 +1,19 @@
 const express = require("express");
 const app = express();
 const router = require("./router");
-
+const cors = require("cors");
 // db connection
 const MongooseConfig = require("./config/MongooseConfig");
 const mongoose = new MongooseConfig();
 mongoose.connect();
 
+const corsOptions = {
+  origin: ["*", "http://localhost:3000"],
+};
+
 app.use(express.urlencoded({ extended: true }));
 app.use(express.json());
+app.use(cors(corsOptions));
 
 app.use(require("morgan")("dev"));
 app.use("/api/v1", router);
