c5b6bf451a8c033938951cfcd067d294a02f948a
Tekdey <devw.nbardi@gmail.com>
2022-09-29 15:59:11 +0200

    fix: cors

--- src/app.js
+++ src/app.js
@@ -8,12 +8,14 @@ const mongoose = new MongooseConfig();
 mongoose.connect();
 
 const corsOptions = {
-  origin: ["*", "http://localhost:3000"],
+  origin: "*",
+  credentials: true, //access-control-allow-credentials:true
+  optionSuccessStatus: 200,
 };
 
+app.use(cors(corsOptions)); // Use this after the variable declarations
 app.use(express.urlencoded({ extended: true }));
 app.use(express.json());
-app.use(cors(corsOptions));
 
 app.use(require("morgan")("dev"));
 app.use("/api/v1", router);
