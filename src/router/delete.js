007401f3154f1018977021b70db9b875e19bf7c9
jmussardev <jmussard.dev@gmail.com>
2022-10-06 16:05:39 +0200

    routes documented

--- src/router/delete.js
+++ src/router/delete.js
@@ -1,6 +1,13 @@
 const router = require("express").Router();
 const { message } = require("../controller");
 
+/**
+ * DELETE /api/v1/message/:id/delete"
+ * @summary Route to delete a message
+ * @tags Message
+ * @return {object} 200 - success response - application/json
+ * @return {string} 400 - Bad request
+ */
 router.delete("/message/:id/delete", message.deleteMessageById);
 
 module.exports = router;
